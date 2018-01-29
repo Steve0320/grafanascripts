e5aa4030f0a2e979474f6abf068204907abd4eda
Steven Bertolucci <srbertol@mtu.edu>
2018-01-29 13:00:52 -0800

    Add Selenium-based AT&T scraper (requires headless Chrome)

--- att.py
+++ att.py
@@ -1,31 +1,55 @@
-import requests
+import re
+import sys
+
 from selenium import webdriver
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-
 from selenium.webdriver.chrome.options import Options
 
+from selenium.common.exceptions import TimeoutException
+
+if len(sys.argv) != 3:
+    print('Usage: att.py <username> <password>')
+    exit(0)
+
+# Launch a headless Chrome session to scrape
 chrome_options = Options()
-#chrome_options.add_argument("--headless")
-#chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1366x768")
 
 driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.get('https://www.att.com/my/#/login')
-#driver.find_element_by_id("userName").send_keys("test")
+wait = WebDriverWait(driver, 20)
 
+driver.get('https://www.att.com/my/#/login')
+
+# Wait until login form loaded
 try:
-    # Wait until login form loaded
-    WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.ID, 'login')))
+    wait.until(EC.presence_of_element_located((By.ID, 'login')))
 except TimeoutException:
     driver.quit()
     exit(-1)
 
-# Submit keys
-driver.find_element_by_id('userName').send_keys('')
-driver.find_element_by_class_name('lgwgPassword').send_keys('')
+# Submit login credentials
+driver.find_element_by_id('userName').send_keys(sys.argv[1])
+driver.find_element_by_class_name('lgwgPassword').send_keys(sys.argv[2])
 driver.find_element_by_id('loginButton').click()
 
-# Add in exception for stupid AT&T popup stuff
+# Wait for main accounts page to load
+try:
+    wait.until(EC.url_to_be('https://www.att.com/my/#/accountOverview'))
+    usage_str = wait.until(EC.presence_of_element_located((By.XPATH, "//span[@ng-if='viewModel.pu_usage_cato']"))).text
+except TimeoutException:
+    driver.quit()
+    exit(-1)
+
+# Extract usage data
+match = re.fullmatch('(\d+\.\d+) of (\d+\.\d+).+', usage_str)
+usage = float(match.group(1))
+total = float(match.group(2))
+
+print(usage/total*100)
+
+driver.quit()
